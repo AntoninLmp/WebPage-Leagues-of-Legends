4cbc3b169c20c3e4a8b0f53f9d62733f5e2b70c1
Simon Martinenq <simon.martinenq@efrei.net>
2021-11-27 19:57:50 +0100

    link between pages
    +page.route file
    + partials header
    + partial footer

--- server.js
+++ server.js
@@ -1,33 +1,28 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const app = express()
-dotenv.config()
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+dotenv.config();
+
+const session = require("express-session");
+app.use(session({
+    secret: "SecretRandomStringDskghadslkghdlkghdghaksdghdksh",
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 }, // 1 day in msec
+    resave: false
+}));
+
 app.listen(process.env.WEB_PORT,
-    function() { console.log("Listening on " + process.env.WEB_PORT) })
-
-app.set("view engine", "ejs")
-app.set("views", "views")
-    //MIDDLEWARE REGISTRATION
-    //app.use(callback1, callback2, callback3)
-    //app.use(routeBase, callbak)
-    //inutile enfin a voir comment mieux faire
-    /* app.use("/site", require("./controllers/pages.routes")) */
-
-app.get('/aboutUs', (req, res) => {
-    res.render('pages/aboutUs')
-})
-app.get('/champions', (req, res) => {
-    res.render('pages/champions')
-})
-app.get('/', (req, res) => {
-    res.render('pages/home')
-})
-app.get('/team', (req, res) => {
-    res.render('pages/team')
-})
+    function() { console.log("Listening on " + process.env.WEB_PORT); }
+);
 
 
+app.set("view engine", "ejs");
+app.set("views", "views");
+// MIDDLEWARE REGISTRATIONS!
+// app.use(callback1, callback2, callback3)
+// app.use(routeBase, callback)
+app.use("/", require("./controllers/page.route"));
 
-const bodyParser = require("body-parser")
-app.use(bodyParser.json(), bodyParser.urlencoded({ extended: true }))
-app.use("/static", express.static(__dirname + '/static'))
+const bodyParser = require("body-parser");
+app.use(bodyParser.json(), bodyParser.urlencoded({ extended: true }));
+app.use("/static", express.static(__dirname + '/static'));
